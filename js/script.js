3b49d67ca0e94c3c19c4e2764fc81ddda608b492
mariannacoppa <densimetra@gmail.com>
2024-06-25 15:53:44 +0200

    [UPD] create price variable without discount

--- js/script.js
+++ js/script.js
@@ -5,3 +5,6 @@ console.log("km");
 // Permetto l'inserimento da parte dell'utente dell'età del passeggero
 let eta = prompt("Inserisci la tua età");
 console.log("età");
+// Creo variabile a cui assegno come valore il prodotto tra il valore km ed il valore età
+let prezzo = (km * 0.21);
+console.log(prezzo);
