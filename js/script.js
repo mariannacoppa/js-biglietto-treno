fcf9405a74eca1fb8d865cd9049bc17155d7c58f
mariannacoppa <densimetra@gmail.com>
2024-06-26 10:14:40 +0200

    [FIX] correction after live coding

--- js/script.js
+++ js/script.js
@@ -6,24 +6,30 @@ console.log("km");
 let eta = prompt("Inserisci la tua età");
 console.log("età");
 // Creo variabile a cui assegno come valore il prodotto tra il valore km ed il valore età
-let prezzo = (km * 0.21);
-let perc_sconto = 0;
-let totale = prezzo * perc_sconto / 100;
-// arrotondo il valore alla seconda cifra decimale
-let finale = totale.toFixed(2);
+let prezzo_base = km * 0.21;
+// let prezzo_finale = prezzo_base * perc_sconto / 100;
+let prezzo_finale = prezzo_base;
 // programmo gli sconti
 if (eta < 18) {
-    perc_sconto = 20;
+    let sconto = prezzo_base * 0.2;
+    prezzo_finale = prezzo_base - sconto;
+    // è la stessa cosa scrivere al posto delle due righe sopra: 
+    // prezzo_finale = prezzo_finale * 0.8;
 }
 else if (eta > 64) {
-    perc_sconto = 40;
+    let sconto = prezzo_base * 0.4;
+    prezzo_finale = prezzo_base - sconto;
+    // prezzo_finale = prezzo_base * 0.6;
 }
-if (perc_sconto != 0) {
-    // prezzo = prezzo * perc_sconto / 100;
-    console.log(totale);
-}
-else if (perc_sconto == 0) {
-    console.log(totale);
-}
+console.log(prezzo_finale);
+// if (sconto != 0) {
+//     // prezzo_base = prezzo_base * sconto / 100;
+//     console.log(prezzo_finale);
+// }
+// else if (sconto == 0) {
+//     console.log(prezzo_finale);
+// }
+// arrotondo il valore alla seconda cifra decimale
+let finale = prezzo_finale.toFixed(2);
 // recupero l'elemento html che contiene la stringa generata e gli assegno il valore della riga 11
 document.getElementById("finale").innerText = finale;
